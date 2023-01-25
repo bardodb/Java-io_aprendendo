03a1d06ab9ec2ea183c6b0d78f607d8b2c84e2be
Braian Sins <bsins198@gmail.com>
2023-01-25 17:20:38 -0300

    formatar números e usar o Locale

---
+++ src/br/com/teste/java/io/teste/TesteUnicodeEEnconding.java
@@ -0,0 +1,24 @@
+package br.com.teste.java.io.teste;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+
+public class TesteUnicodeEEnconding {
+  public static void main(String[] args) throws UnsupportedEncodingException {
+
+    String s = "C";
+    System.out.println(s.codePointAt(0));
+
+
+    Charset charset = Charset.defaultCharset();
+    System.out.println(charset.displayName());
+
+    byte[] bytes = s.getBytes("windows-1252");
+    System.out.println(bytes.length + ", windows-1252 ");
+
+    bytes = s.getBytes("UTF-16");
+    System.out.println(bytes.length + ", UTF-16");
+
+
+  }
+}
